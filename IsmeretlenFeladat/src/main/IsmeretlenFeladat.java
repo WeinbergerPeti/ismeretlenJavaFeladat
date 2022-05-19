c8b902cbcc57985462fd5414618f30b4f6774c8b
WeinbergerPeti <weinbergerpeti@gmail.com>
2022-05-19 14:41:40 +0200

    Első alap állapot

---
+++ IsmeretlenFeladat/src/main/IsmeretlenFeladat.java
@@ -0,0 +1,10 @@
+package main;
+
+public class IsmeretlenFeladat {
+
+    public static void main(String[] args) 
+    {
+        
+    }
+    
+}
---
+++ IsmeretlenFeladat/src/main/TorpedoTeszt.java
@@ -0,0 +1,14 @@
+package main;
+
+public class TorpedoTeszt 
+{
+    public String tesztLoves(int poz)
+    {
+        return "";
+    }
+    
+    public static void main(String[] args) 
+    {
+        
+    }
+}
